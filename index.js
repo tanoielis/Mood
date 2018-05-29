0fcec3367181e88dc28b8c0a6969762d1da7659f
tanoielis <eli.tanoi@gmail.com>
2018-05-29 19:33:37 +1200

    Started moving routes to own folder

--- index.js
+++ index.js
@@ -56,54 +56,7 @@ mongodb.MongoClient.connect(uri, {useNewUrlParser:true}, (err, db) => {
 
     app.use(express.urlencoded({extended: true}));
     
-    app.get('/', function(req, res) {
-            dbo.collection('moods').find().sort({createdAt: -1}).toArray(function (err, docs) {
-                if (err) {
-                        console.log(err);
-                } else {
-                    if (docs) {
-                        let moods = [];
-                        docs.forEach(function (mood) {
-                            if ('createdAt' in mood === false) {
-                                console.error('Depricated mood in database ' + JSON.stringify(mood));
-                                return;
-                            }
-                            let date = mood.createdAt;
-                            let start = new Date (date.getTime());
-                            start.setHours(mood.start.substr(0, 2));
-                            let end = new Date(start.getTime());
-                            end.addHours(2);
-                            moods.push('<li class="list-group-item d-flex justify-content-between align-items-center">Time: ' + start.getHours() + '-' + end.getHours() + ', ' + date.toDateString() + '<span class="badge badge-primary badge-pill">' + mood.mood + '</span></li>');
-                        });
-                        let page = indexStart + moods.join(' ') + indexEnd;
-                        res.send(page);
-                    }
-                }
-            });
-            
-        });
-    
-    app.post('/', function(req, res) {
-            if ('moodRating' in req.body && 'startTime' in req.body) {
-                let moodRating = req.body.moodRating;
-                let startTime = req.body.startTime;
-                let date = new Date();
-                console.log(date);
-                let options = {mood:moodRating,start:startTime,createdAt:date};
-                dbo.collection('moods').insert(options, function (err) {
-                        if(err) {
-                            console.log(err);
-                        }
-                    });
-            }
-            res.redirect('/');
-        });
-    
-    app.post('/delete', function (req, res) {
-            let deletables = req.body.delete || {};
-            dbo.collection('moods').remove(deletables, false);
-            res.redirect('/');
-        });
+   
     
         app.listen(port, () => console.log(`Started server on port ${port}!`));
 });
---
+++ routes/router.js
@@ -0,0 +1,90 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const User = require('../models/userSchema.js');
+
+router.get('/', function(req, res) {
+    return res.sendFile(path.join(__dirname + '/../authenticate.html'));
+});
+
+router.post('/', function(req, res, next) {
+    // handle log ins
+    if ('user' in req.body && 'password' in req.body) {
+        User.authenticate(req.body.user, req.body.password, function(error, user) {
+            if(error || !user) {
+                let err = new Error('Wrong email or password.');
+                err.status = 401;
+                return next(err);
+            } else {
+                req.session.userId = user._id;
+                return res.redirect('/profile');
+            }
+        });
+    } else {
+        let err = new Error('All fields required.');
+        err.status = 400;
+        return next(err);
+    }            
+});
+
+router.get('/profile', function(req, res, next) {
+    User.findById(req.session.userId);
+        .exec(function (error, user) {
+            if (error) {
+                return next(error);
+            } else {
+                if (user === null) {
+                    let err = new Error('Not authorized!');
+                    err.status = 400;
+                    return next(err);
+                } else {
+                    return res.send(
+
+router.get('/mood', function(req, res) {
+            dbo.collection('moods').find().sort({createdAt: -1}).toArray(function (err, docs) {
+                if (err) {
+                        console.log(err);
+                } else {
+                    if (docs) {
+                        let moods = [];
+                        docs.forEach(function (mood) {
+                            if ('createdAt' in mood === false) {
+                                console.error('Depricated mood in database ' + JSON.stringify(mood));
+                                return;
+                            }
+                            let date = mood.createdAt;
+                            let start = new Date (date.getTime());
+                            start.setHours(mood.start.substr(0, 2));
+                            let end = new Date(start.getTime());
+                            end.addHours(2);
+                            moods.push('<li class="list-group-item d-flex justify-content-between align-items-center">Time: ' + start.getHours() + '-' + end.getHours() + ', ' + date.toDateString() + '<span class="badge badge-primary badge-pill">' + mood.mood + '</span></li>');
+                        });
+                        let page = indexStart + moods.join(' ') + indexEnd;
+                        res.send(page);
+                    }
+                }
+            });
+            
+        });
+    
+router.post('/mood', function(req, res) {
+            if ('moodRating' in req.body && 'startTime' in req.body) {
+                let moodRating = req.body.moodRating;
+                let startTime = req.body.startTime;
+                let date = new Date();
+                console.log(date);
+                let options = {mood:moodRating,start:startTime,createdAt:date};
+                dbo.collection('moods').insert(options, function (err) {
+                        if(err) {
+                            console.log(err);
+                        }
+                    });
+            }
+            res.redirect('/mood');
+        });
+    
+router.post('/mood/delete', function (req, res) {
+            let deletables = req.body.delete || {};
+            dbo.collection('moods').remove(deletables, false);
+            res.redirect('/mood');
+        });
